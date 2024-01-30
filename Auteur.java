0e4b7c29635fa84e082cc047da23abc6056b82e8
EtuIckae <mickael.riego@etu.univ-orleans.fr>
2024-01-30 16:25:34 +0100

    constructeur auteur

--- Auteur.java
+++ Auteur.java
@@ -8,11 +8,22 @@ public class Auteur{
     private String perf_dram;
 
 
-    public void Auteur(String nom,int point_trag,  String perf_trag, int point_com, String perf_com, int point_dram, String perf_dram){
+    public Auteur(String nom,int point_trag,  String perf_trag, int point_com, String perf_com, int point_dram, String perf_dram){
         this.nom = nom;
         this.point_trag = point_trag;
         this.perf_trag = perf_trag;
 
+        this.point_com = point_com;
+        this.perf_com = perf_com;
+
+        this.point_dram = point_dram;
+        this.perf_dram = perf_dram;
+
+    }
+
+    @Override
+    public String toString(){
+        return "L'honorable " + this.nom;
     }
     
 }
