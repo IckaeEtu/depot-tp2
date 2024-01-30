b9c98b4fa60da8d2149bf90dfbdb696646797efa
EtuIckae <mickael.riego@etu.univ-orleans.fr>
2024-01-30 16:15:01 +0100

    aa

---
+++ Auteur.java
@@ -0,0 +1,18 @@
+public class Auteur{
+    private String nom;
+    private int point_trag;
+    private String perf_trag;
+    private int point_com;
+    private String perf_com;
+    private int point_dram;
+    private String perf_dram;
+
+
+    public void Auteur(String nom,int point_trag,  String perf_trag, int point_com, String perf_com, int point_dram, String perf_dram){
+        this.nom = nom;
+        this.point_trag = point_trag;
+        this.perf_trag = perf_trag;
+
+    }
+    
+}
